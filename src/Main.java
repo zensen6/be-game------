9ba831bbd63e8b43ed1dd54710aae33333e80760
zensen6 <junhee1469@gmail.com>
2023-12-29 15:54:51 +0900

    final commit

--- src/Main.java
+++ src/Main.java
@@ -10,15 +10,18 @@ import static java.lang.Math.min;
 public class Main {
 
     static int N;
-    static int []count_ = new int [14];
-    static int []sum;
-    static boolean []done;
+    static int[] count_ = new int[14];
+    static int[] sum;
+    static boolean[] done;
+
+
+    static boolean[] finish;
     static int c = 0;
 
     static String yes;
 
-    static int shuffle(){
-        while(true) {
+    static int shuffle() {
+        while (true) {
             Random rand = new Random();
             int iValue = rand.nextInt(13) + 1;
             if (count_[iValue] < 4) {
@@ -37,22 +40,24 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Enter the number of Player participating the game. Your number is the last number of all player.");
         N = scanner.nextInt();  // # of player
-        sum = new int[N+1];
-        done = new boolean[N+1];
-        for(int i=1;i<=N;i++){
+        sum = new int[N + 1];
+        done = new boolean[N + 1];
+        finish = new boolean[N + 1];
+        for (int i = 1; i <= N; i++) {
             sum[i] = 0;
             done[i] = false;
+            finish[i] = false;
         }
         int s;
         int turn = 0;
-        while(true) {
+        while (true) {
 
             turn++;
             if (turn == 1) {
                 for (int i = 1; i <= N - 1; i++) {
                     int ret = shuffle();
                     sum[i] += ret;
-                    System.out.println("Player " + i + "'s score: " + sum[i]);
+                    System.out.println("Player " + i + " chose " + ret+ ", score: " + sum[i]);
                 }
                 System.out.println("It's your turn. Choose whether pick up the card or not. Y/N");
                 yes = br.readLine();
@@ -60,14 +65,14 @@ public class Main {
                     int ret = shuffle();
                     sum[N] += ret;
                     c++;
-                    System.out.println("Player " + N + "'s score: " + sum[N]);
+                    System.out.println("You chose " + ret + ", score: " + sum[N]);
                 } else {
                     done[N] = true;
                 }
 
             } else {
                 for (int i = 1; i <= N - 1; i++) {
-                    if(done[i]) continue;
+                    if (finish[i] || done[i]) continue;
                     int target = (21 - sum[i]);
                     s = 0;
                     for (int j = 1; j <= min(target, 13); j++) {
@@ -81,15 +86,17 @@ public class Main {
                         int ret = shuffle();
                         sum[i] += ret;
                         if (sum[i] > 21) {
-                            done[i] = true;
+                            finish[i] = true;
                             System.out.println("Player " + i + "'s score: " + sum[i] + " is over 21!");
                         } else {
-
-                            System.out.println("Player " + i + "'s score: " + sum[i]);
+                            if (sum[i] == 21) {
+                                System.out.println("Player " + i + "'s Score is 21, Win!!");
+                                return;
+                            } else System.out.println("Player " + i + " chose " + ret + ", score: " + sum[i]);
                         }
                     }
                 }
-                if(done[N]) continue;
+                if (done[N] || finish[N]) continue;
                 System.out.println("It's your turn. Choose whether pick up the card or not. Y/N");
                 yes = br.readLine();
                 if (yes.equals("Y")) {
@@ -97,41 +104,62 @@ public class Main {
                     sum[N] += ret;
                     c++;
                     if (sum[N] > 21) {
-                        done[N] = true;
+                        finish[N] = true;
                         System.out.println("Your score: " + sum[N] + " is over 21!");
                     } else {
+                        if (sum[N] == 21) {
+                            System.out.println("Your score is 21. You Win!!");
+                            return;
+                        }
                         System.out.println("Player " + N + "'s score: " + sum[N]);
                     }
 
                 } else {
                     done[N] = true;
                 }
+
+
                 int p = 0;
-                for (int k = 1; k <= N; k++) if (done[k] == false) p++;
-                if (p == 1) {
+                int active = 0;
+                for (int k = 1; k <= N; k++) {
+                    if (finish[k] == false && done[k] == true) p++;
+                    if (done[k] == false && finish[k] == false) active++;
+                }
+
+                if (active == 0 && (p > 0)) { // 안죽었는데 done은 있음
+                    int m = 21;
+                    int index = 0;
                     for (int k = 1; k <= N; k++) {
-                        if (done[k] == false) {
-                            System.out.println("Player " + k + " win!");
-                            break;
+                        if (finish[k] == false && done[k] == true) {
+                            if (m > 21 - sum[k]) {
+                                m = 21 - sum[k];
+                                index = k;
+                            }
+                        }
+                    }
+                    System.out.println("Player " + index + " win by Score: " + sum[index]);
+                    return;
+                }
+
+
+                else if(p == 1 || active == 1) {
+                    for (int k = 1; k <= N; k++) {
+                        if (finish[k] == false) {
+                            System.out.println("Player " + k + " win! He is the last survivor");
+                            return;
                         }
                     }
                     return;
                 }
-                else if(p==0){
-                    System.out.println("Tie!!");
-                    return;
-                }
             }
         }
-            // N 일때
+        // N 일때
 
-            System.out.println("hello");
-        }
 
     }
 
 
-
+}
 
 /*
 public static string Personality(int n) {
