9de6857860d754135f4cc7e55a5ab38d9b9bd29e
jjyk999 <jjyk999@gmail.com>
2023-12-29 14:11:34 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,32 @@
 import java.util.*;
+import java.util.
 
 
 public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
+        int N = scanner.nextInt();  // # of player
         int []count_ = new int [14];
         int []sum = new int[N];
-
-
-
     }
 }
+
+
+
+/*
+public static string Personality(int n) {
+
+
+}
+
+
+
+
+
+
+
+
+
+
+ */
