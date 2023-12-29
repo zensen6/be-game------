65aaf1c2070a26bcf81efd5b354408dcd7c8eded
zensen6 <junhee1469@gmail.com>
2023-12-29 13:41:07 +0900

    first

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int []count_ = new int [14];
+        int []sum = new int[N];
+
+
+
+    }
+}
