74f1ff0be34a52ab5e21afc7deb38106ecc6cee1
zensen6 <junhee1469@gmail.com>
2023-12-29 14:20:06 +0900

    second

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import java.util.
 
 
 public class Main {
@@ -8,7 +7,28 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();  // # of player
         int []count_ = new int [14];
-        int []sum = new int[N];
+        int []sum = new int[N+1];
+        boolean []done = new boolean[N+1];
+        int c = 0,s;
+        while(true){
+
+            for(int i=1;i<=N-1;i++){
+                int target = (21 - sum[i]);
+                s = 0;
+                for(int j=1;j<=target;j++){
+                    s += (4 - count_[j]);
+                }
+                if((double)((s)/(52 - c)) < 0.5){
+                    done[i] = true;
+                    System.out.println("Player " + i + " done!");
+                }else{
+
+                }
+            }
+
+
+        }
+
     }
 }
 
